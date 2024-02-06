58e2e027f7fc68ec055fce0703a173513d2a5def
Lucaturin2003 <159005233+Lucaturin2003@users.noreply.github.com>
2024-02-06 09:38:06 +0100

    Add files via upload

---
+++ AppOggetti/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AppOggetti
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthdate { get; private set; }
+
+        public Person(DateTime birthday, string name, string surname)
+        {
+            Birthdate = birthday;
+            Name = name;
+            Surname = surname;
+        }
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365; 
+                return years;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persona = new Person(new DateTime(2003, 5, 2), "Luca", "Turin");
+            Console.WriteLine(persona.Age);
+            Console.WriteLine(persona.Name);
+            Console.WriteLine(persona.Surname);
+        }
+    }
+
+}
